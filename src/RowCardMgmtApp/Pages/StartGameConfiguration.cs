a559e4880bece5b47ca2ec2127b273ede9fe398f
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2021-01-17 18:49:45 +0100

    SAVEPOINT

---
+++ src/RowCardMgmtApp/Pages/StartGameConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RowCardMgmtApp.Pages
+{
+    public class StartGameConfiguration
+    {
+        [Required]
+        public string Token { get; set; }
+
+        [Range(typeof(int), "1", "4", ErrorMessage = "Wert {0} muss zwischen {1} und {2} sein")]
+        public int NumberOfPlayers { get; set; }
+
+        [Range(typeof(int), "1", "4", ErrorMessage = "Wert {0} muss zwischen {1} und {2} sein")]
+        public int NumberOfRealPlayers { get; set; }
+    }
+}
